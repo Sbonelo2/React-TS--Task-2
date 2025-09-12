19ad48111cc3d02255604d7faf5154fed933e63f
Sbonelo2 <cbonelo224@gmail.com>
2025-09-12 12:59:54 +0200

    i remove the unwanted Components

--- src/Components/InpuTt.tsx
+++
@@ -1,9 +0,0 @@
-// import Tabled from './Tabled'
-// export default function Input({ text, ...rest }) {
-//   return (
-//     <div>
-//       <label>{text}</label>
-//       <input {...rest} />
-//     </div>
-//   );
-// }
